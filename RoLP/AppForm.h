443117a4b6252c898959a4c3bb9eea43b046a3ff
Bartlomiej Pyk <pyk.bartlomiej@gmail.com>
2018-11-28 21:57:25 +0100

    Create and add MetroFramework.

--- RoLP/AppForm.h
+++ RoLP/AppForm.h
@@ -8,11 +8,12 @@ namespace RoLP {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace MetroFramework::Forms;
 
 	/// <summary>
 	/// Summary for AppForm
 	/// </summary>
-	public ref class AppForm : public System::Windows::Forms::Form
+	public ref class AppForm : public MetroForm
 	{
 	public:
 		AppForm(void)
@@ -48,12 +49,24 @@ namespace RoLP {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"AppForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->SuspendLayout();
+			// 
+			// AppForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BorderStyle = MetroFramework::Forms::MetroFormBorderStyle::FixedSingle;
+			this->ClientSize = System::Drawing::Size(1344, 565);
+			this->Name = L"AppForm";
+			this->Style = MetroFramework::MetroColorStyle::Red;
+			this->Text = L"AppForm";
+			this->Theme = MetroFramework::MetroThemeStyle::Dark;
+			this->Load += gcnew System::EventHandler(this, &AppForm::AppForm_Load);
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
+	private: System::Void AppForm_Load(System::Object^  sender, System::EventArgs^  e) {
+	}
 	};
 }
